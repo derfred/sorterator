a9b3c089be920207aac54f61de3ddde9ce93f098
Frederik Fix <ich@derfred.com>
2011-01-25 19:18:29 +0100

    admin work

--- admin.js
+++ admin.js
@@ -2,8 +2,19 @@ function AdminClient(port) {
   
 }
 
-AdminClient.prototype.update_users = function() {
-  $.getJSON("/users", function(data) {
-    console.log(data.length)
-  });
+AdminClient.prototype.update_players = function() {
+  $.getJSON("/players", $.proxy(this.update_players_div, this));
+}
+
+AdminClient.prototype.update_players_div = function(players) {
+  $(".player_lists").hide();
+  if(players.length == 0) {
+    $("#empty_player_list").show();
+  } else {
+    $("#player_list").show();
+
+    for(var i=0;i<players.length;i++) {
+      $("<li>"+players[i].name+" <a href='"+players[i].id+"'>Kick</a></li>").appendTo("#player_list");
+    }
+  }
 }
